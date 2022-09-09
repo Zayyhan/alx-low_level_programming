ca3c74f4be941e9c23e58991d7f2845d50ea3d27
Zayyhan <Zayyhanb2@gmail.com>
2022-09-08 22:08:14 -0700

    Coding.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.>
+#include <stdio.h>
 
+/**
+* program prints the string inside the block
+* return 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
