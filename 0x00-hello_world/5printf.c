8bcaedfe5a694e6ae549e3bae303d44af9fb1250
Zayyhan <Zayyhanb2@gmail.com>
2022-09-09 08:38:32 -0700

    printf.c

---
+++ 0x00-hello_world/5printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program prints string set by printf
+ * return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
