24c56e7c1a32c23f3e78c8ae51cd6abf0ee4dc32
Zayyhan <Zayyhanb2@gmail.com>
2022-09-12 02:11:17 -0700

    print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,20 @@
 /**
 * main - Entry point
 *
-* Description: this program prints lowercase alphabet
-*
-* Return : Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
+
 {
-char alphabet = 'a';
+char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 putchar(alphabet);
 
 putchar('\n');
 
 return (0);
-
 }
