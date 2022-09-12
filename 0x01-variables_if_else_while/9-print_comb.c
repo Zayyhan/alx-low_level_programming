33c624a6b7fd63b59752ac9b56eadea7fa2e2569
Zayyhan <Zayyhanb2@gmail.com>
2022-09-12 07:53:22 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-int num;
+char num;
 
 for (num = '0'; num <= '9'; num++)
+putchar((num % 10) + '0');
+if (num == '9')
 putchar(',');
-purchar(' ');
-putchar(num);
+putchar(' ');
 
 putchar('\n');
 
 return (0);
-}
