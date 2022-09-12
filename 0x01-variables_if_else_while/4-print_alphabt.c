0a28cfeb18875a625da261c007329d258d46569d
Zayyhan <Zayyhanb2@gmail.com>
2022-09-12 03:21:42 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,11 @@ char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet != 'e' && alphabet != 'q')
-}
+{
 putchar(alphabet);
-
-ptuchar('\n');
+}
+}
+putchar('\n');
 
 return (0);
 }
