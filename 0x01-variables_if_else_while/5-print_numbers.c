710a0c94c8cf5ecc885f60cc803f5f46811580ed
Zayyhan <Zayyhanb2@gmail.com>
2022-09-12 03:51:42 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-char num;
+int num;
 
-for (num = 0; num < 9; num++)
-putchar(num);
+for (num = 0; num < 10; num++)
+printf("%d", num);
 
-putchar('\n');
+printf('\n');
 
 return (0);
 }
