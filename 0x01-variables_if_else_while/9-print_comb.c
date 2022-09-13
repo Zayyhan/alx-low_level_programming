fe35fbfa1efc3eeeb35f9f5fa25bdb0b64517a73
Zayyhan <Zayyhanb2@gmail.com>
2022-09-12 20:20:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,15 @@
 */
 int main(void)
 {
-int d1, d2, n = 0;
+int n;
 
-for (d1 = '0'; d1 <= '9'; d1++)
-for (d2 = d1 + 1; d2 <= '9'; d2++, n++)
-if (n > 0)
+n = 0;
+while (n < 9)
+putchar(n);
+
 putchar(',');
 putchar(' ');
-
-putchar(d1);
-putchar(d2);
+putchar('\n');
 
 return (0);
 }
