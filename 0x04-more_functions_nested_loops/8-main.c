e326865068bd6041e1b57fdead263fa3ebc8693b
Zayyhan <Zayyhanb2@gmail.com>
2022-09-16 09:11:03 -0700

    print_square.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -4,12 +4,11 @@
 * main - check the code
 *
 * Return: Always 0.
-
+*/
 int main(void)
 {
-print_diagonal(0);
-print_diagonal(2);
-print_diagonal(10);
-print_diagonal(-4);
+print_square(2);
+print_square(10);
+print_square(0);
 return (0);
 }
