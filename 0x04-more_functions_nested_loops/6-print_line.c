6f1fe6536d56dbb783be82e85400cd644675a85c
Zayyhan <Zayyhanb2@gmail.com>
2022-09-16 04:30:11 -0700

    print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - prints straight line
+*
+* @n: int used for parameter of the function
+* Return: void
+*/
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+
+return;
+}
