0909a95bdb8da9e03497f03df95ead425fd8b0cd
Zayyhan <Zayyhanb2@gmail.com>
2022-09-22 09:43:51 -0700

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -79,13 +79,3 @@ int main(void)
     return (0);
 
 }
-
-julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-rev_array.c -o 4-rev_array
-
-julien@ubuntu:~/0x06$ ./4-rev_array 
-
-0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337
-
-1337, 1024, 98, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-
-julien@
