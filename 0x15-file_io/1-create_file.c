a5f8aaa4aba67b9234bc4a64c8572f5b69bc44f1
Zayyhan <Zayyhanb2@gmail.com>
2022-10-31 21:03:22 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-        if (fd == -1)
+	if (fd == -1)
 		return (-1);
 
 	for (length = 0; text_content[length]; length++)
